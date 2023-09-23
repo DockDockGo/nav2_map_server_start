b7ab1ee5f2a98618744bdc4ea9e59a072777bb27
sushanthj <sushanth.jayanth@gmail.com>
2023-09-23 19:15:10 -0400

    made some progress, still not working

--- launch/nav2_map_server_start.launch.py
+++ launch/nav2_map_server_start.launch.py
@@ -5,34 +5,50 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
+from nav2_common.launch import RewrittenYaml
+from ament_index_python.packages import get_package_prefix
 import launch_ros.actions
 
 
 def generate_launch_description():
 
     ld = LaunchDescription()
+    params_file = default=os.path.join(
+            get_package_prefix('nav2_map_server_start'),
+            'maps/', 'map_server_params.yaml')
+    print("looking for params file in", params_file)
+    
+    map_file = default=os.path.join(
+            get_package_prefix('nav2_map_server_start'),
+            'maps/', 'map.yaml')
 
-    # Map server
-    map_server_config_path = os.path.join(
-        get_package_share_directory('nav2_map_server_start'),
-        'maps',
-        'map_server_params.yaml'
-    )
-
-
-
-    map_server_cmd = Node(
-        package='nav2_map_server',
-        executable='map_server',
-        output='screen',
-        parameters=[{'use_sim_time': False, 'map_file': str(map_server_config_path)}])
+    parameters =  LaunchConfiguration('params_file', default=params_file)
+    namespace = LaunchConfiguration('namespace', default='')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    autostart = LaunchConfiguration('autostart', default='true')
 
 
     lifecycle_nodes = ['map_server']
-    use_sim_time = False
-    autostart = True
 
-    start_lifecycle_manager_cmd = launch_ros.actions.Node(
+    configured_params = RewrittenYaml(
+        source_file=parameters, # note source_file is the simple yaml file
+        root_key=namespace,
+        param_rewrites={'use_sim_time' : use_sim_time, 'yaml_filename': map_file}, # note yaml_filename is the one associated with the .pgm
+        convert_types=True)
+
+    # remapppings = ('what the node expects', 'what it should expect in reality')
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
+
+    map_server_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        output='screen',
+        parameters=[configured_params],
+        remappings=remappings)
+
+
+    map_lifecycle_node = launch_ros.actions.Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
             name='lifecycle_manager',
@@ -45,7 +61,7 @@ def generate_launch_description():
 
 
 
-    ld.add_action(map_server_cmd)
-    ld.add_action(start_lifecycle_manager_cmd)
+    ld.add_action(map_server_node)
+    ld.add_action(map_lifecycle_node)
 
     return ld
